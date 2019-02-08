a8bad11dc8842c30cf0edd4d3501db29bacea8fd
Effy Fan <effyfan@10-17-195-217.dynapool.wireless.nyu.edu>
2019-02-07 20:21:26 -0500

    assignment 2 first commit

---
+++ assign2/ass2/config.h
@@ -0,0 +1,9 @@
+const char WIFI_SSID[] = "itpsandbox";
+const char WIFI_PASSWORD[] = "NYU+s0a!+P?";
+
+const char MQTT_BROKER[] = "iotwork.shop";
+const int MQTT_PORT = 8883;
+const char MQTT_USER[] = "device_ef";
+const char MQTT_PASSWORD[] = "giguye_542";
+
+const String DEVICE_ID = "device_ef";
